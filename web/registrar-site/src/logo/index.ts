df1253a9ae26dd016f19a927ba1d02ecc9b0012d
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-11 09:31:35 -0800

    move logos to src folder

---
+++ web/registrar-site/src/logo/index.ts
@@ -0,0 +1,15 @@
+import account from "./account.png";
+import bookmarks from "./bookmarks.png";
+import bookmarksCloudfront from "./bookmarks-cloudfront.png";
+import default_ from "./default.png";
+import feedshare from "./feedshare.svg";
+import local from "./local.png";
+
+export const logo: Record<string, any> = {
+  account,
+  bookmarks,
+  "bookmarks-cloudfront": bookmarksCloudfront,
+  default: default_,
+  feedshare,
+  local,
+};
